a4590ce39100fa0a02b4390496ac25b2fe88f478
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2021-03-07 14:49:42 +0100

    switched to monospaced font

--- Typing Jumper/Assets/Scripts/LetterBox.cs
+++ Typing Jumper/Assets/Scripts/LetterBox.cs
@@ -6,6 +6,7 @@ public class LetterBox : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI letterText;
     [SerializeField] Image background;
+    [SerializeField] float extraSizeOnX;
 
     [SerializeField] Color correctColor;
     [SerializeField] Color incorrectColor;
@@ -17,9 +18,10 @@ public class LetterBox : MonoBehaviour
         var rect = gameObject.GetComponent<RectTransform>();
 
         var size = rect.sizeDelta;
-        size.x = letterText.preferredWidth;
-
+        size.x = letterText.preferredWidth + extraSizeOnX;
         rect.sizeDelta = size;
+
+        size.y = rect.sizeDelta.y;
     }
 
     public void SetLetterColor(bool isCorrect)
